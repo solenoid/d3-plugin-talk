c4c545710d489516074fb03447383ef89068f45b
Erik Solen <erikshaner@gmail.com>
2012-11-05 00:23:12 -0500

    separate top and side labels from data

--- plugins/matrix.js
+++ plugins/matrix.js
@@ -39,39 +39,32 @@ define(['jquery', 'lodash', 'd3', 'jquery-ui'], function ($, _, d3) {
 
         // Data is assumed to be a 2 dimensional array with all inner arrays the same length.
         // An appropriate data max is assumed to be passed in too.
-        // Labels for the top headers are the first row.
-        // Labels for individual rows are the first element in the inner arrays.
-        // All labels and values will be html escaped.
+        // Labels for the top and side are separate from the data and will be html escaped.
         _init: function () {
             var self = this,
                 tableHeaderRow,
                 tableRows,
-                // first row has header labels html escaped
-                escapedTopHeaderData = _.map(_.first(self.options.data), function (label) { return _.escape(label); }),
-                // the rest has data starting with row labels
-                data = _.rest(self.options.data),
-                dataWidth = escapedTopHeaderData.length,
+                labelEscaper = function (label) { return '<th>' + _.escape(label) + '</th>'; },
+                escapedTopHeaders = _.map(self.options.topLabels, labelEscaper),
+                escapedSideHeaders = _.map(self.options.sideLabels, labelEscaper),
+                data = self.options.data,
+                dataWidth = data[0].length,
                 dataHeight = data.length,
                 colorScale = d3.scale.linear()
                     .domain([0, self.options.max])
                     .range([0, 1]),
                 table = self.element.append('<table></table').find('table:last');
             self.formatter = self.options.format || self.kFormat;
-            tableHeaderRow = '<tr class="labels"><th>' +
-                escapedTopHeaderData.join('</th><th>') +
-                '</th></tr>';
+            tableHeaderRow = '<tr>' + escapedTopHeaders.join('') + '</tr>';
             tableRows = _.map(data, function (row, height_index) {
-                var cells = _.map(row, function (val, width_index) {
+                var rowLabel = escapedSideHeaders[height_index];
+                var dataCells = _.map(row, function (val, width_index) {
                     var scaledColor,
                         colorVal = val,
                         firstCell = (width_index === 0),
                         lastCell = (width_index === (dataWidth - 1)),
                         lastRow = (height_index === (dataHeight - 1)),
                         cellWidthStyle = 'min-width:' + self.options.cellWidth + ';';
-                    // treat the first element in the array as a label
-                    if (firstCell) {
-                        return '<th>' + _.escape(val) + '</th>';
-                    }
                     // don't show null or undefined values
                     if (val != null) {
                         // scale color value down when it is an aggregated overall
@@ -88,7 +81,7 @@ define(['jquery', 'lodash', 'd3', 'jquery-ui'], function ($, _, d3) {
                     }
                     return ('<td style="' + cellWidthStyle + 'background:#fff;">&mdash;</td>');
                 }).join('\n');
-                return '<tr class="labels">' + cells + '</tr>';
+                return '<tr>' + rowLabel + dataCells + '</tr>';
             }).join('\n');
             table.append(tableHeaderRow + tableRows);
         }
